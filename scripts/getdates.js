5b0385ccd86987faf60a503d1a98b851e3f7bdda
LordOz2dMOON <oswaldojo@ymail.com>
2025-05-11 03:34:09 +0100

    wdd131 home page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get the current year for copyright
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Get the last modified date of the document
+document.getElementById('lastModified').textContent = 'Last modification: ' + document.lastModified;
